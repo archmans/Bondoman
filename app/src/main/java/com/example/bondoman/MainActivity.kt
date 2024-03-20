40ee9e0b141143336ad43eee512c60b0dd86ee66
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-03-20 15:46:21 +0700

    feat(jwt): can resume service jwt

--- app/src/main/java/com/example/bondoman/MainActivity.kt
+++ app/src/main/java/com/example/bondoman/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.bondoman
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bondoman.services.JWTExpiry
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var service: Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        service = Intent(this, JWTExpiry::class.java)
+        startService(service)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        startService(Intent(this, JWTExpiry::class.java))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(service)
     }
 }
