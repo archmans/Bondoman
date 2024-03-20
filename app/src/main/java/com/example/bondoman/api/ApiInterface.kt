7d14bbe331e22773b3301679d112b01a004a7ea3
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-03-20 15:44:56 +0700

    feat(jwt): api auth token

--- app/src/main/java/com/example/bondoman/api/ApiInterface.kt
+++ app/src/main/java/com/example/bondoman/api/ApiInterface.kt
@@ -2,10 +2,15 @@ package com.example.bondoman.api
 
 import com.example.bondoman.models.LoginRequest
 import com.example.bondoman.models.LoginResponse
+import com.example.bondoman.models.JWTResponse
 import retrofit2.http.POST
 import retrofit2.http.Body
+import retrofit2.http.Header
 
 interface ApiInterface {
     @POST("/api/auth/login")
     suspend fun login(@Body loginRequest: LoginRequest): LoginResponse
+
+    @POST("/api/auth/token")
+    suspend fun jwt(@Header("Authorization") token: String): JWTResponse
 }
