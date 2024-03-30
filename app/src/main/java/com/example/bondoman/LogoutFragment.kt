32645bb567696269e221ada084da002cd0d4cc12
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-03-30 14:58:34 +0700

    feat(logout): little fix

--- app/src/main/java/com/example/bondoman/LogoutFragment.kt
+++ app/src/main/java/com/example/bondoman/LogoutFragment.kt
@@ -9,20 +9,31 @@ import androidx.fragment.app.Fragment
 import android.content.Intent
 
 class LogoutFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_logout, container, false)
-        val buttonLogout = view.findViewById<Button>(R.id.logoutButton)
-        buttonLogout.setOnClickListener {
-            val sharedPreferences = requireActivity().getSharedPreferences("sharedPrefs", MODE_PRIVATE)
-            val editor = sharedPreferences.edit()
-            editor.clear().apply()
-            val intent = Intent(requireActivity(), LoginActivity::class.java)
-            startActivity(intent)
-            requireActivity().finish()
+
+        view.findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            logout()
         }
         return view
+    }
+
+    private fun logout() {
+        clearUserSession()
+        val loginIntent = Intent(requireContext(), LoginActivity::class.java)
+        loginIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(loginIntent)
+    }
+
+    private fun clearUserSession() {
+        val sharedPreferences = requireContext().getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+
+        val editor = sharedPreferences.edit()
+        editor.clear()
+        editor.apply()
     }
 }
