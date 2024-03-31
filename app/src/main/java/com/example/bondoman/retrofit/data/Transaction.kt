e3d9e0f52e67523f92816ab70acb69b43b7d845f
Laila Bilbina <13521016@std.stei.itb.ac.id>
2024-03-31 16:30:58 +0700

    add TransactionActivity

---
+++ app/src/main/java/com/example/bondoman/retrofit/data/Transaction.kt
@@ -0,0 +1,37 @@
+package com.example.bondoman.retrofit.data
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Transaction(val name: String?, val date: String?, val category: String?, val price: Int?, val location: String?) :
+    Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(date)
+        parcel.writeString(category)
+        parcel.writeValue(price)
+        parcel.writeString(location)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Transaction> {
+        override fun createFromParcel(parcel: Parcel): Transaction {
+            return Transaction(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Transaction?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
